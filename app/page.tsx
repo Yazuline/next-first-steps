b9de95b9f7c54c31ee1543b6e0d90d549bd183c4
Viveros Bedoya Yazuline <Yazuline.Viveros@carvajal.com>
2024-02-13 11:12:36 -0500

    Creacion link navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex flex-col items-center p-24">
-     <span className="text-5xl"> Hola Mundo</span>
+     <span className="text-5xl"> First Step Nex💥 </span>
+     <a href='https://next-first-steps-three-teal.vercel.app/about'>Vista Nabvar</a>
     </main>
   )
 }
