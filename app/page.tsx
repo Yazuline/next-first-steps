3046e7a0a827aefe267e708d41818e563261c234
Viveros Bedoya Yazuline <Yazuline.Viveros@carvajal.com>
2024-02-13 11:30:24 -0500

    cambio titulos

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex flex-col items-center p-24">
-     <span className="text-5xl"> First Step Nex💥 </span>
-     <a href='https://next-first-steps-three-teal.vercel.app/about'>Vista Nabvar</a>
+     <span className="text-5xl"> First Step Next💥 </span>
+     <br />
+     <a href='https://next-first-steps-three-teal.vercel.app/about'>💫Aquí Vista Nabvar💫</a>
     </main>
   )
 }
